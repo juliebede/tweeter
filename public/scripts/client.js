5d1b1d648991a5ea58432eb56ecb5bea8bb1fcbc
Julie Nguyen <julie-nguyen@live.com>
2019-12-19 00:44:58 +0000

    Add feature to slide form up and down

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -37,68 +37,91 @@ function formatTime(timeCreated) {
 }
 
 
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET'})
+  .then(function(data) {
+    $("#tweetsContainer").empty();
+    renderTweets(data);
+  })
+}
+
+
+
+// Renders data into HTML
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    const newTweet = createTweetElement(tweet);
+    $("#tweetsContainer").prepend(newTweet);
+  } 
+}
+
+// Creates the article using given data
+const createTweetElement = function(tweetObject) {
+  // Initialization of each child element and it's class
+  let $tweet = $("<article>");
+  let $header = $("<header>").addClass("tweetHeader");
+  let $tweetText = $("<section>").addClass("description");
+  let $footer = $("<footer>").addClass("tweetFooter");
+
+  //Appended data details for each child
+  $header.append(`<img class="headerAvatar" src="${tweetObject.user.avatars}">
+                  <p class="name">${tweetObject.user.name}</p>
+                  <p class="handle">${tweetObject.user.handle}</p>`);
+
+  $tweetText.append($("<p>").text(tweetObject.content.text));
+  
+  const time = formatTime(tweetObject.created_at);
+  $footer.append(`<p class="timestamp">${time}</p>`);
+  $footer.append(`<p class="icons">
+                  <span>&#9873</span>
+                  <span> &#10562 </span>
+                  <span> &#10084 </span>
+                  </p>`)
+  
+  // Appended all children to article
+  $tweet.append($header).append($tweetText).append($footer);
+  return $tweet;
+}
+
 $(document).ready(function () {
-  const loadTweets = function() {
-    $.ajax('/tweets', {method: 'GET'})
-    .then(function(data) {
-      renderTweets(data);
-    })
-  }
+
   loadTweets()
 
   const $tweetButton = $('#tweetForm');
+
   $tweetButton.submit(function( event ) {
-    if ($(this.childNodes[5]).text() < 0) {
-      alert('Tweet is too long.');
-    } else if ($(this.childNodes[5]).text() == 140) {
+    console.log(`{ ${$tweetButton.serialize()} }`)
+    const formButton = ($(this).find($("span")))
+    event.preventDefault();
+    if ($(formButton).text() < 0) {
+      let $error = $($(this).parent()).find("#errorMessage");
+      $error.text("Tweet is too long.")
+    } else if ($(formButton).text() == 140) {
       alert('Tweet is empty.')
     } else {
+      console.log('julie')
       $.ajax({
         url: /tweets/, // url where to submit the request
         type : "POST", 
-        dataType : 'json', // data type
+        dataType : 'text', // data type
         data : $tweetButton.serialize(), // post data
+      }).done(function() {
+          loadTweets()
       })
-      loadTweets();
-      $($tweetButton[0].childNodes[1]).val('');
+      $($(this).find($("#tweet"))).val('');  // makes input empty again
+      $($(this).find($(".counter"))).text('140'); // brings counter back to 140
     }
-    event.preventDefault();
-  });  
+  });
 
-
-  // Renders data into HTML
-  const renderTweets = function(tweets) {
-    for (let tweet of tweets) {
-      const newTweet = createTweetElement(tweet);
-    $("#tweetsContainer").prepend(newTweet);
-    } 
-  }
-
-  // Creates the article using given data
-  const createTweetElement = function(tweetObject) {
-    // Initialization of each child element and it's class
-    let $tweet = $("<article>");
-    let $header = $("<header>").addClass("tweetHeader");
-    let $tweetText = $("<section>").addClass("description");
-    let $footer = $("<footer>").addClass("tweetFooter");
-
-    //Appended data details for each child
-    $header.append(`<img class="headerAvatar" src="${tweetObject.user.avatars}">
-                    <p class="name">${tweetObject.user.name}</p>
-                    <p class="handle">${tweetObject.user.handle}</p>`);
-
-    $tweetText.append(`<p>${tweetObject.content.text}</p>`);
-    
-    const time = formatTime(tweetObject.created_at);
-    $footer.append(`<p class="timestamp">${time}</p>`);
-    $footer.append(`<p class="icons">
-                    <span>&#9873</span>
-                    <span> &#10562 </span>
-                    <span> &#10084 </span>
-                    </p>`)
-    
-    // Appended all children to article
-    $tweet.append($header).append($tweetText).append($footer);
-    return $tweet;
-  }
+  // Slides the form up and down using arrow on top right
+  const $formSlidingButton = $(document).find('#arrow');
+  $formSlidingButton.click(function(event) {
+    const form = $(this).parent().siblings(".container").find(".new-tweet");
+    if ($(form).attr("style") === "display: none;") {
+      $(form).slideDown();
+    } else {
+      $(form).slideUp();
+    }
+  })
 });
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $("#tweet").keyup(function() {
     let counter = $(this).val().length;
     let newVal = 140 - counter;
-    $(this.parentNode.childNodes[5]).text(newVal);
+    $($(this).siblings('span')).text(newVal);
 
     // Toggle between classes for negative/positive values
     if (newVal < 0) {
-      $(this.parentNode.childNodes[5]).addClass('negativeCounter').removeClass('counter');
+      $($(this).siblings('span')).addClass('negativeCounter').removeClass('counter');
     } else {
-      $(this.parentNode.childNodes[5]).removeClass('negativeCounter').addClass('counter');
+      $($(this).siblings('span')).removeClass('negativeCounter').addClass('counter');
     }
   });
 });
